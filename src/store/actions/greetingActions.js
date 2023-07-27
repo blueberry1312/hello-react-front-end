9e58cea21c21852fde4702c11ee880c317c9e732
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-07-27 13:01:11 -0400

    Configure store, reducers, actions and component

---
+++ src/store/actions/greetingActions.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const fetchGreeting = () => {
+  return (dispatch) => {
+    dispatch(fetchGreetingRequest());
+    axios
+      .get('http://localhost:3000/greetings') 
+      .then((response) => {
+        const greeting = response.data.name;
+        dispatch(fetchGreetingSuccess(greeting));
+      })
+      .catch((error) => {
+        dispatch(fetchGreetingFailure(error.message));
+      });
+  };
+};
+
+export const fetchGreetingRequest = () => ({
+  type: 'FETCH_GREETING_REQUEST',
+});
+
+export const fetchGreetingSuccess = (greeting) => ({
+  type: 'FETCH_GREETING_SUCCESS',
+  payload: greeting,
+});
+
+export const fetchGreetingFailure = (error) => ({
+  type: 'FETCH_GREETING_FAILURE',
+  payload: error,
+});
